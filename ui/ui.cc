1b7589d93f6f0091f05dac3d9c571aa9d3fbe649
2hf <88843953+2hf@users.noreply.github.com>
2022-10-30 10:02:08 +0200

    Add files via upload

---
+++ ui/ui.cc
@@ -0,0 +1,33 @@
+#include "ui.hh"
+#include "../imgui/imgui.h"
+#include "../imgui/imgui_internal.h"
+
+static bool active = true;
+void ui::render() {
+    if (!active)return;
+    ImGui::SetNextWindowPos(ImVec2(window_pos.x, window_pos.y), ImGuiCond_Once);
+    ImGui::SetNextWindowSize(ImVec2(window_size.x, window_size.y));
+    ImGui::SetNextWindowBgAlpha(1.0f);
+
+    ImGui::Begin(window_title, &active, window_flags);
+    {
+
+    }
+    ImGui::End();
+}
+
+void ui::init(LPDIRECT3DDEVICE9 device) {
+    dev = device;
+	
+    // colors
+    ImGui::StyleColorsDark();
+
+	if (window_pos.x == 0) {
+		RECT screen_rect{};
+		GetWindowRect(GetDesktopWindow(), &screen_rect);
+		screen_res = ImVec2(float(screen_rect.right), float(screen_rect.bottom));
+		window_pos = (screen_res - window_size) * 0.5f;
+
+		// init sprites here
+	}
+}
